0cfb481e81cfe10107fa4d097738f7b0c1c1d886
wlgnsld1108 <wlgnsld1108@naver.com>
2021-08-09 19:00:30 +0900

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,34 @@
 (function() {
   "use strict";
 
+  // cursor
+  const blobCursor = function() {
+    const cursor = document.querySelector('#cursor-blob');
+    const link = document.querySelector('.nav-menu > ul, #portfolio-flters');
+    
+    const setCursorPos = (e) => {
+      let posX = e.pageX;
+      let posY = e.pageY;
+      
+      cursor.style.top = posY - (cursor.offsetHeight/2) + 'px';
+      cursor.style.left = posX - (cursor.offsetWidth/2) + 'px';
+    }
+    
+    document.addEventListener('mousemove', setCursorPos);
+    
+    const setCursorHover = function() {
+      cursor.style.transform = 'scale(1.6)';
+    }
+    const removeCursorHover = function() {
+      cursor.style.transform = '';
+    }
+    
+    link.addEventListener('mouseover', setCursorHover);
+    link.addEventListener('mouseleave', removeCursorHover);
+  }
+  
+  blobCursor();
+
   /**
    * Easy selector helper function
    */
@@ -252,4 +280,5 @@
       mirror: false
     })
   });
+
 })()
